f5c4d301919fdf01492fa4fed32f414f56b50980
Edmilson Soares <esbnet@gmail.com>
2022-11-03 22:44:23 -0300

    feat: :sparkles: create context and hook Auth

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
 import {
-  Roboto_400Regular, Roboto_500Medium,
-  Roboto_700Bold, useFonts
-} from '@expo-google-fonts/roboto';
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto"
+import { StatusBar } from "expo-status-bar"
+import { NativeBaseProvider } from "native-base"
 
-import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
+import { Loading } from "./src/components/Loading"
+import { AuthContextProvider } from "./src/contexts/AuthContext"
+import { SignIn } from "./src/screens/SignIn"
 
-import { THEME } from './src/styles/theme';
+import { THEME } from "./src/styles/theme"
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Roboto_400Regular, Roboto_500Medium,
-    Roboto_700Bold
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
   })
 
   return (
-    < NativeBaseProvider theme={THEME}>
-      <StatusBar style="inverted" translucent />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+    <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
+        <StatusBar style='inverted' translucent />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
-  );
+  )
 }
